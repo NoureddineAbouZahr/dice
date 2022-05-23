b64e00ebdbc6a2cba8b15d81db93572b14b27b8f
Noureddine Abou Zahr <naz.199860@gmail.com>
2022-05-23 12:30:53 +0300

    adding winning status

--- main.js
+++ main.js
@@ -1,27 +1,19 @@
-let x = 0;
-let y = 0;
 function randomDice() {
-    setTimeout(() => {
-        x = Math.floor(Math.random() * 6) + 1;
-        y = Math.floor(Math.random() * 6) + 1;
-        document.querySelector(".img1").setAttribute("src", "./images/dice" + x + ".png");
-        document.querySelector(".img2").setAttribute("src", "./images/dice" + y + ".png");
-
-
-    }, 1000);
+    let x = Math.floor(Math.random() * 6) + 1;
+    let y = Math.floor(Math.random() * 6) + 1;
+    document.querySelector(".img1").setAttribute("src", "images/dice" + x + ".png");
+    document.querySelector(".img2").setAttribute("src", "images/dice" + y + ".png");
     if (x > y) {
-        document.querySelector(".status").innerHTML = "Player 1 Wins"
+        document.querySelector(".status").innerHTML = "&#128681; Player 1 Wins";
 
     }
     else if (x < y) {
-        document.querySelector(".status").innerHTML = "Player 2 Wins"
+        document.querySelector(".status").innerHTML = "&#128681; Player 2 Wins";
 
     }
     else {
-        document.querySelector(".status").innerHTML = "Draw"
+        document.querySelector(".status").innerHTML = "Draw";
     }
-
-
 }
 randomDice();
 
